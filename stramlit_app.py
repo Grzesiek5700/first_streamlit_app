f7764efab5bebee629a5ece1c84f513c71bad691
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-20 11:50:28 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents Healty Diner')
 
 streamlit.header('Breakfast Menu')
-#stremalit.text('Omega 3  Blueberry Oatmeal')
+stremalit.text('Omega 3 & Blueberry Oatmeal')
 #stremalit.text('Kale, Spinach & Rocket Smoothie')
 #stremalit.text('Hard_Boiled Free_Range Egg')
