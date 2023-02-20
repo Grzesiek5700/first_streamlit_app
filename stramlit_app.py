8dcf816f519aad1445cdf3ed624e7713566b3129
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-20 17:47:38 +0100

    Update stramlit_app.py
    added pandas and df

--- stramlit_app.py
+++ stramlit_app.py
@@ -5,5 +5,11 @@ st.title('My Parents New Healty Diner')
 st.header('Breakfast Favorites')
 st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
-st.text('🥑🍞Hard_Boiled Free_Range Egg')
+st.text('🐔 Hard_Boiled Free_Range Egg')
+st.text('🥑🍞Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+df = st.dataframe(my_fruit_list)
