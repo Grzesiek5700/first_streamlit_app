71564b9bf3f97f2a675ab3aa2b25f90dc1ba05da
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-20 11:54:21 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -4,5 +4,5 @@ st.title('My Parents Healty Diner')
 
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
-#stremalit.text('Kale, Spinach & Rocket Smoothie')
-#stremalit.text('Hard_Boiled Free_Range Egg')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard_Boiled Free_Range Egg')
