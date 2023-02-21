91ddb2d8f3626da5002a0c9e708ed2527ffc46e0
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-21 16:46:19 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -20,6 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 df = st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
+
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
