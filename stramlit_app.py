18db23785c0b974bea5472aa0b4dd99dc1854e9f
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-21 16:41:16 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -27,4 +27,4 @@ st.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
