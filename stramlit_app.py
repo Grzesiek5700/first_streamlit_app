d126cf101dff26d7f76826398ff9579dfa1054fc
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-22 11:36:11 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("fruit_load_list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.multiselect("what fruit would you like to add:", list(my_fruit_list.index))
-added_fruits_to_show = my_fruit_list.loc[fruit_selected]
+add_my_fruit = streamlit.text_input("what fruit would you like to add:")
+streamlit.write('The user entered ', add_my_fruit)
