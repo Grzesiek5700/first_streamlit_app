06e1ef2803eba4ffe45122b52a6a8a5ff44e7f50
Grzesiek5700 <125872931+Grzesiek5700@users.noreply.github.com>
2023-02-22 11:11:46 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,28 +1,28 @@
-import streamlit as st
+import streamlit
 
-st.title('My Parents New Healty Diner')
+streamlit.title('My Parents New Healty Diner')
 
-st.header('Breakfast Favorites')
-st.text('🥣Omega 3 & Blueberry Oatmeal')
-st.text('🥗Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard_Boiled Free_Range Egg')
-st.text('🥑🍞Avocado Toast')
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard_Boiled Free_Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruit_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 
-df = st.dataframe(fruits_to_show)
+df = streamlit.dataframe(fruits_to_show)
 
-st.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
@@ -31,15 +31,15 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 
 import snowflake.connector
-import streamlit
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
